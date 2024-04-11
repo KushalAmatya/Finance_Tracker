7012c9d8f839357f63ce07a732337d33b1537e02
Kushal Amatya <kushalamatya413@gmail.com>
2024-04-11 13:17:56 +0545

    Updated home with dialog logout

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,26 +1,141 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'dart:js';
 
 import 'package:finance_tracker/authscreen/auth_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
 
   final user = FirebaseAuth.instance.currentUser!;
+
   void signUserOut() {
     FirebaseAuth.instance.signOut();
+  }
+
+  void logOut(BuildContext context) {
+    showDialog(
+        // Remove the 'context' argument
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: Container(
+              height: 400,
+              width: 350,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  "You're Logged in as:",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  user.email!,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ])
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Log Out?",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      signUserOut();
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Yes"),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("No"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))],
-      ),
-      body: Center(
-        child: Text("logged in" + user.email!),
-      ),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          leading: Icon(Icons.trending_up_rounded, size: 40),
+          backgroundColor: Color.fromRGBO(215, 178, 157, 1),
+          actions: [
+            Text(user.email!),
+            IconButton(
+                onPressed: signUserOut, icon: Icon(Icons.logout, size: 40))
+          ],
+        ),
+        bottomNavigationBar: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(215, 178, 157, 1),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.home, size: 40),
+                Icon(Icons.add, size: 40),
+                IconButton(
+                  onPressed: () {
+                    logOut(context);
+                  },
+                  icon: Icon(Icons.account_circle, size: 40),
+                  // Icon(Icons.account_circle, size: 40),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
